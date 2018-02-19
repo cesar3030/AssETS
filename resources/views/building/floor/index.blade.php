c0490a55c1a456571b0806d2bf1d3745cc469bf2
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2018-02-18 19:32:39 -0500

    floor breadcrumbs

--- resources/views/building/floor/index.blade.php
+++ resources/views/building/floor/index.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            {{ Breadcrumbs::render('building_dynamic', $building) }}
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <div class="row">
