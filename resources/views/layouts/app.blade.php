5ec3a652a1f569d46f3d900226bf21ec3688aa1e
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2018-03-19 21:45:49 -0400

    last touches

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>AssETS</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        AssETS
                     </a>
                 </div>
 
@@ -43,12 +43,12 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">Connexion</a></li>
+                            <li><a href="{{ route('register') }}">Créer un compte</a></li>
                         @else
-                            <li><a href="{{ route('user.index') }}">Users</a></li>
+                            <li><a href="{{ route('user.index') }}">Utilisateurs</a></li>
                             <li><a href="{{ route('equipment.index') }}">Équipements</a></li>
-                            <li><a href="{{ route('building.index') }}">Building</a></li>
+                            <li><a href="{{ route('building.index') }}">Batiments</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
